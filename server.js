8c4fdf881a1104e11fd00de420728f3dee8e198b
Subho Karmakar <isubhokarma@gmail.com>
2019-05-28 01:54:06 +0530

    npm run dev -- concurrently

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ mongoose
 
 app.use("/api/items", items);
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
